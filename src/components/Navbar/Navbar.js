27e088f11e9fcdec79f29dcf87a11dac7a7158ae
cwt1101 <cwas0516@gmail.com>
2022-06-02 09:22:15 +0900

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import "../../App.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+            <div className="container">
+                <Link to="/" className="navbar-brand fs-3 ubuntu">
+                    Rick & Morty <span className="text-primary">WiKi</span>
+                </Link>
+                <style jsx="true">
+                    {`
+                        button[aria-expanded="false"] >. close {
+                            width: 100%;
+                            height: 100%;
+                            display: block;
+                        }
+                        button[aria-expanded="true"] >. open {
+                            width: 100%;
+                            height: 100%;
+                            display: block;
+                        }
+                    `}
+                </style>
+                <button
+                    className="navbar-toggler border-2"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="fas fa-bars open text-dark"></span>
+                    <span className="fas fa-times close text-dark"></span>
+                </button>
+                <div
+                    className="collapse navbar-collapse justify-content-end"
+                    id="navbarNavAltMarkup"
+                >
+                    <div className="navbar-nav fs-5">
+                        <NavLink to="/" className="nav-link">
+                            Characters
+                        </NavLink>
+                        <NavLink to="/episodes" className="nav-link">
+                            Episode
+                        </NavLink>
+                        <NavLink activeclassname="active" className="nav-link" to="/location">
+                            Location
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
